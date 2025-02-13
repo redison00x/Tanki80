223fd629e41970c4071e878ffd77d829553fb9eb
Mathew Cgit config --global user.email <praimebotob@gmail.com>
2025-02-13 16:22:20 +0300

    7commit

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -20,6 +20,8 @@ public class Game implements Runnable {
     private Input             input;
     private TextureAtlas      atlas;
     private Player            player;
+    private Level lvl;
+
 
     public Game(){
         running = false;
@@ -29,6 +31,7 @@ public class Game implements Runnable {
         Display.addInputListener(input);
         atlas = new TextureAtlas(ATLAS_FILE_NAME);
         player = new Player(300,300,atlas,2, 3);
+        lvl = new Level(atlas);
     }
 
     public synchronized void start(){
@@ -52,10 +55,12 @@ public class Game implements Runnable {
 
     private void update(){
         player.update(input);
+        lvl.update();
     }
 
     private void render(){
         Display.clear();
+        lvl.render(graphics);
         player.render(graphics);
         Display.swapBuffers();
     }
--- src/Level/Level.java
+++ src/Level/Level.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Level {
     public static final int TILE_SCALE = 8;
-    public static final int TILE_IN_GAME_SCALE = 1;
+    public static final int TILE_IN_GAME_SCALE = 2;
     public static final int SCALE_TILE_SIZE = TILE_SCALE * TILE_IN_GAME_SCALE;
     public static final int TILES_IN_WIDTH = Game.WIDTH / SCALE_TILE_SIZE;
     public static final int TILES_IN_HEIGHT = Game.HEIGHT / SCALE_TILE_SIZE;
@@ -15,32 +15,28 @@ public class Level {
 
     public Level(TextureAtlas atlas) {
         tileMap = new int[TILES_IN_WIDTH][TILES_IN_HEIGHT];
-        tiles = new HashMap<>();
-        tiles.put(TileType.BRICK, new Tile(atlas.cut
-                (32*TILE_SCALE, 0*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.BRICK));
+        tiles = new HashMap<TileType,Tile>();
 
-        tiles.put(TileType.METAL, new Tile(atlas.cut
-                (32*TILE_SCALE, 2*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.METAL));
+        tiles.put(TileType.BRICK, new Tile(atlas.cut(33*TILE_SCALE, 0*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.BRICK));
 
-        tiles.put(TileType.WATER, new Tile(atlas.cut
-                (32*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.WATER));
+        tiles.put(TileType.METAL, new Tile(atlas.cut(33*TILE_SCALE, 2*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.METAL));
 
-        tiles.put(TileType.GRASS, new Tile(atlas.cut
-                (34*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.GRASS));
+       tiles.put(TileType.WATER, new Tile(atlas.cut(33*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.WATER));
 
-        tiles.put(TileType.ICE, new Tile(atlas.cut
-                (36*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.ICE));
+       tiles.put(TileType.GRASS, new Tile(atlas.cut(35*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.GRASS));
 
-        tiles.put(TileType.EMPTY, new Tile(atlas.cut
-                (36*TILE_SCALE, 6*TILE_SCALE, TILE_SCALE, TILE_SCALE),
-                TILE_IN_GAME_SCALE,TileType.EMPTY)); //пустота
+        tiles.put(TileType.ICE, new Tile(atlas.cut(37*TILE_SCALE, 4*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.ICE));
+
+        tiles.put(TileType.EMPTY, new Tile(atlas.cut(44*TILE_SCALE, 6*TILE_SCALE, TILE_SCALE, TILE_SCALE), TILE_IN_GAME_SCALE,TileType.EMPTY)); //пустота
 
         tileMap = new int[TILES_IN_WIDTH][TILES_IN_HEIGHT];
+
+        tileMap[10][10] = TileType.BRICK.numeric();
+        tileMap[11][11] = TileType.WATER.numeric();
+        tileMap[10][12] = TileType.METAL.numeric();
+        tileMap[13][13] = TileType.GRASS.numeric();
+        tileMap[14][14] = TileType.ICE.numeric();
+
     }
 
     public void update() {
@@ -50,7 +46,8 @@ public class Level {
     public void render(Graphics2D g) {
         for(int i = 0; i < tileMap.length; i++) {
             for(int j = 0; j < tileMap[i].length; j++) {
-                tiles.get(tileMap[j][i]).render(g,j*SCALE_TILE_SIZE,i*SCALE_TILE_SIZE);
+                tiles.get(TileType.fromNumeric(tileMap[i][j]))
+                        .render(g,j*SCALE_TILE_SIZE,i*SCALE_TILE_SIZE);
             }
         }
 
